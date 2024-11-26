5b72193d713284e47557a9aab63098e2f587f6fc
ulysseyson <ulysseyson57@gmail.com>
2024-11-26 23:35:21 +0900

    feat: video rendering package & code

--- render_video.py
+++ render_video.py
@@ -4,31 +4,41 @@ import os
 from tqdm import tqdm
 
 # Directory containing the PNG files
-p = "02_straight_duck_walk"
-image_folder = f"tmp/data/{p}/pcd"
-output_file = f"{p}.mp4"
+ps = [
+    "01_straight_walk",
+    "02_straight_duck_walk",
+    "03_straight_crawl",
+    "04_zigzag_walk",
+    "05_straight_duck_walk",
+    "06_straight_crawl",
+    "07_straight_walk",
+]
 
-# Get all PNG files in the directory, sorted by filename
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-images.sort()  # Ensure the images are in order
+for p in ps:
+    image_folder = f"tmp/try2/{p}"
+    output_file = f"tmp/try2/{p}.mp4"
 
-# Read the first image to determine the frame size
-first_image_path = os.path.join(image_folder, images[0])
-frame = cv2.imread(first_image_path)
-height, width, _ = frame.shape
+    # Get all PNG files in the directory, sorted by filename
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    images.sort()  # Ensure the images are in order
 
-# Define video codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Use 'XVID' for .avi files
-fps = 30  # Frames per second
-out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+    # Read the first image to determine the frame size
+    first_image_path = os.path.join(image_folder, images[0])
+    frame = cv2.imread(first_image_path)
+    height, width, _ = frame.shape
 
-# Loop through images and write them to the video
-for image in tqdm(images):
-    image_path = os.path.join(image_folder, image)
-    frame = cv2.imread(image_path)
-    out.write(frame)
+    # Define video codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Use 'XVID' for .avi files
+    fps = 15  # Frames per second
+    out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
 
-# Release the VideoWriter object
-out.release()
+    # Loop through images and write them to the video
+    for image in tqdm(images):
+        image_path = os.path.join(image_folder, image)
+        frame = cv2.imread(image_path)
+        out.write(frame)
 
-print(f"Video saved as {output_file}")
+    # Release the VideoWriter object
+    out.release()
+
+    print(f"Video saved as {output_file}")
